9d991372f8e843e8ea0ac5620647985d538bb219
shiro-kreishi <vadas25@yandex.ru>
2024-04-15 23:53:22 +0500

    first eaxmple form

---
+++ src/f1/get/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>f1</title>
+</head>
+<body>
+<form action="action.php" method="get">
+    <label for="name">Ваше имя:</label>
+    <input name="name" id="name" type="text">
+    <label for="surname">Ваша фамилия:</label>
+    <input name="surname" id="surname" type="text">
+    <label for="email">email</label>
+    <input type="text" name="email" id="email">
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
+
+
+
+<?php
